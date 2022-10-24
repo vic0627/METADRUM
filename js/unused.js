2dc853de5d2db3ac00da287ff5b6c17e208c0679
pure90719@gmail.com <pure90719@gmail.com>
2022-10-25 02:09:05 +0800

    Initial commit

---
+++ js/unused.js
@@ -0,0 +1,23 @@
+// Rain Drop Texture
+        /* let rainCount=9500;
+        let cloudParticles=[];
+        const rainGeo = new THREE.BoxGeometry(1, 1, 1);
+        for(let i=0; i<rainCount; i++) {
+            const rainDrop = new THREE.Vector3(
+                Math.random()*400-200,
+                Math.random()*500-250,
+                Math.random()*400-200
+            )
+            rainDrop.velocity = {};
+            rainDrop.velocity = 0;
+            rainGeo.vertices.push(rainDrop);
+        }
+
+        const rainMaterial = new THREE.PointsMaterial({
+            color: 0xaaaaaa,
+            size: 0.1,
+            transparent: true
+        })
+
+        const rain = new THREE.Points(rainGeo, rainMaterial);
+        scene.add(rain) */
